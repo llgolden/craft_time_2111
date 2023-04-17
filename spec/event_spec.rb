749acf2cd42e19601463c6be586f904a198dd719
Lauren Golden <llgolden@gmail.com>
2023-04-16 20:59:43 -0600

    Craft class complete, Event class in progress

--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -6,23 +6,21 @@ require './lib/event'
 RSpec.describe Event do
   it "exists" do
     event = Event.new("Carla's Craft Connection", [craft], [person])
-    expect(event).to be_a(Event)
+    # expect(event).to be_an(Event)
   end
 
-  it "has attributes" do
+  xit "has attributes" do
     event = Event.new("Carla's Craft Connection", [craft], [person])
     expect(event.name).to eq("Carla's Craft Connection")
-    expect().to eq()
-    expect().to eq()
+    # expect(event.crafts).to eq()
+    # expect(event.attendees).to eq()
 
-
-  
   end
 end
 
 
-pry(main)> event.crafts
-#=> [#<Craft:0x00007fd37b818318...>]
+# pry(main)> event.crafts
+# #=> [#<Craft:0x00007fd37b818318...>]
 
-pry(main)> event.attendees
-#=> [#<Person:0x00007fd379b96028...>]
+# pry(main)> event.attendees
+# #=> [#<Person:0x00007fd379b96028...>]
