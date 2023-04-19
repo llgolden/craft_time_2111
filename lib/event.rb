86cb830c0be4430d255f4b37536e2204671ba04f
Lauren Golden <llgolden@gmail.com>
2023-04-19 08:37:51 -0600

    Update files

--- lib/event.rb
+++ lib/event.rb
@@ -7,4 +7,21 @@ class Event
     @attendees = attendees
   end
 
+  def attendee_names
+    attendee_names = []
+    @attendees.each do |attendee|
+      attendee_names << attendee.name
+    end
+    attendee_names
+  end
+
+  def craft_with_most_supplies
+    a = {}
+    crafts.map do |craft|
+      a[craft.name] = craft.supplies_required.count
+    end
+    require 'pry'; binding.pry
+
+  end
+
 end #final
--- spec/event_spec.rb
+++ spec/event_spec.rb
@@ -5,22 +5,73 @@ require './lib/event'
 
 RSpec.describe Event do
   it "exists" do
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']}) 
     event = Event.new("Carla's Craft Connection", [craft], [person])
-    # expect(event).to be_an(Event)
+    expect(event).to be_an(Event)
   end
 
-  xit "has attributes" do
+  it "has attributes" do
+    craft = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    person = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']}) 
     event = Event.new("Carla's Craft Connection", [craft], [person])
     expect(event.name).to eq("Carla's Craft Connection")
-    # expect(event.crafts).to eq()
-    # expect(event.attendees).to eq()
-
+    expect(event.crafts).to eq([craft])
+    expect(event.attendees).to eq([person])
   end
+
+  it "can have multiple attendees" do 
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+    expect(event.attendee_names).to eq(["Hector", "Toni"])
+  end
+
+  it "can determine what craft has the most supplies" do 
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+    
+    expect(event.craft_with_most_supplies).to eq("sewing")
+  end
+
+  xit "can create a supply list" do 
+    hector = Person.new({name: 'Hector', interests: ['sewing', 'millinery', 'drawing']})
+    toni = Person.new({name: 'Toni', interests: ['sewing', 'knitting']})
+    sewing = Craft.new('sewing', {fabric: 5, scissors: 1, thread: 1, sewing_needles: 1})
+    knitting = Craft.new('knitting', {yarn: 20, scissors: 1, knitting_needles: 2})
+    event = Event.new("Carla's Craft Connection", [sewing, knitting], [hector, toni])
+    
+    expect(event.supply_list).to eq(["fabric", "scissors", "thread", "sewing_needles", "yarn", "knitting_needles"])
+  end
+
+
 end
 
 
-# pry(main)> event.crafts
-# #=> [#<Craft:0x00007fd37b818318...>]
+# ## Iteration 2
+# Use TDD to update your classes to respond to the following interaction pattern
+# * `craft_with_most_supplies` returns the name of the craft that requires the most supply types.
 
-# pry(main)> event.attendees
-# #=> [#<Person:0x00007fd379b96028...>]
+
+
+# pry(main)> hector.can_build?(sewing)
+# #=> false
+
+# pry(main)> hector.add_supply('fabric', 7)
+
+# pry(main)> hector.add_supply('thread', 1)
+
+# pry(main)> hector.can_build?(sewing)
+# #=> false
+
+# pry(main)> hector.add_supply('scissors', 1)
+
+# pry(main)> hector.add_supply('sewing_needles', 1)
+
+# pry(main)> hector.can_build?(sewing)
+# #=> true
